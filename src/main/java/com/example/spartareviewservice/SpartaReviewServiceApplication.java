dff33876d1d5e1781cf32bf62d1c3fb3ebcd880a
cykei <kelle111@naver.com>
2024-10-09 11:00:54 +0900

    초기세팅 완성

---
+++ src/main/java/com/example/spartareviewservice/SpartaReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spartareviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaReviewServiceApplication.class, args);
+    }
+
+}
